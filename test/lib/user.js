91fe9c9d3e108c8cc394d27059b7325b5776d685
Luke Bonaccorsi <luke@lukeb.co.uk>
2017-02-12 19:53:41 +0000

    Update user class to have token based auth

--- test/lib/user.js
+++ test/lib/user.js
@@ -4,6 +4,7 @@ const nedb = require(`nedb`);
 const bluebird = require(`bluebird`);
 const path = require(`path`);
 const bcrypt = require(`bcrypt`);
+const crypto = require(`crypto`);
 const sinon = require(`sinon`);
 const chai = require(`chai`);
 const chaiAsPromised = require(`chai-as-promised`);
@@ -74,10 +75,58 @@ describe(`User`, function() {
                     return bluebird.resolve({username: 'test', password});
                 });
 
+                sandbox.stub(user, 'generateToken', function() {
+                    return bluebird.resolve(12345);
+                });
+
                 const userPromise = user.authenticate('test', 'password');
 
-                return expect(userPromise).to.eventually.be.true;
+                return expect(userPromise).to.eventually.equal(12345);
             });
+        });
+    });
+
+    describe(`#verifyToken()`, function(){
+        it(`should reject if the token is invalid`, function() {
+            sandbox.stub(user.userData, 'findOneAsync', function() {
+                return bluebird.resolve();
+            });
+
+            const userPromise = user.verifyToken('invalid-token');
+
+            return expect(userPromise).to.be.rejectedWith(`Invalid token`);
+        });
+
+        it(`should resolve if the token is valid`, function() {
+            sandbox.stub(user.userData, 'findOneAsync', function() {
+                return bluebird.resolve({username: 'test'});
+            });
+
+            const userPromise = user.verifyToken('12345');
+
+            return expect(userPromise).to.eventually.be.true;
+        });
+    });
+
+    describe(`#generateToken()`, function(){
+        it(`should return a token for the user`, function() {
+            sandbox.stub(user.userData, 'updateAsync', function() {
+                return bluebird.resolve();
+            });
+
+            const date = Date.now();
+
+            sandbox.useFakeTimers(date);
+
+            const hash = crypto.createHash(`sha256`);
+
+            hash.update(`test` + date);
+
+            const token = hash.digest(`hex`);
+
+            const userPromise = user.generateToken(`test`);
+
+            return expect(userPromise).to.eventually.equal(token);
         });
     });
 });
